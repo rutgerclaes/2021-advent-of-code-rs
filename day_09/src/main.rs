d1eb850e767b476615c58d67401984dd9a589f55
Rutger Claes <rgc@rgc.be>
2021-12-09 20:41:09 +0100

    Formatting day 9

--- day_09/src/main.rs
+++ day_09/src/main.rs
@@ -1,11 +1,11 @@
+use ansi_term::Color;
 use im_rc::hashset;
 use im_rc::HashMap;
 use im_rc::HashSet;
-use tailcall::tailcall;
 use itertools::Itertools;
 use std::fmt::Display;
-use ansi_term::Color;
 use std::str::FromStr;
+use tailcall::tailcall;
 use utils::input::*;
 use utils::output::*;
 use utils::results::*;
@@ -29,53 +29,83 @@ fn part_one(map: &HeightMap) -> Result<usize> {
 }
 
 fn part_two(map: &HeightMap) -> Result<usize> {
-    
     #[tailcall]
-    fn calculate_basin( map: &HeightMap, seeds: HashSet<(usize,usize)>, basin_positions: HashSet<(usize,usize)> ) -> HashSet<(usize,usize)> {
+    fn calculate_basin(
+        map: &HeightMap,
+        seeds: HashSet<(usize, usize)>,
+        basin_positions: HashSet<(usize, usize)>,
+    ) -> HashSet<(usize, usize)> {
         match seeds.iter().next() {
             None => basin_positions,
-            Some( seed ) => {
-                let next_points: HashSet<(usize,usize)> = map.neighbours_of( &seed.0, &seed.1 ).iter()
-                    .filter( |&(_,h)| *h < 9 )
-                    .map( |(p,_)| p )
-                    .filter( |pos| !basin_positions.contains( pos ) )
+            Some(seed) => {
+                let next_points: HashSet<(usize, usize)> = map
+                    .neighbours_of(&seed.0, &seed.1)
+                    .iter()
+                    .filter(|&(_, h)| *h < 9)
+                    .map(|(p, _)| p)
+                    .filter(|pos| !basin_positions.contains(pos))
                     .copied()
                     .collect();
 
-                calculate_basin( map, seeds.without( seed ).union( next_points ), basin_positions.update( *seed ) )
+                calculate_basin(
+                    map,
+                    seeds.without(seed).union(next_points),
+                    basin_positions.update(*seed),
+                )
             }
         }
     }
 
-    let basins = map.minima().map( |(pos,_)| calculate_basin( map, hashset![ pos ], HashSet::new() ) ).collect();
+    let basins = map
+        .minima()
+        .map(|(pos, _)| calculate_basin(map, hashset![pos], HashSet::new()))
+        .collect();
 
-    colorize( map, &basins );
-    let product = basins.iter().map( |b| b.len() ).sorted_by_key( |&s| -1 * s as i64 ).take( 3 ).product();
-    Ok( product )
+    colorize(map, &basins);
+    let product = basins
+        .iter()
+        .map(|b| b.len())
+        .sorted_by_key(|&s| -1 * s as i64)
+        .take(3)
+        .product();
+    Ok(product)
 }
 
-fn colorize( map: &HeightMap, basins: &Vec<HashSet<(usize,usize)>> ) {
+fn colorize(map: &HeightMap, basins: &Vec<HashSet<(usize, usize)>>) {
     let colours = vec![
-        Color::Blue, Color::Cyan, Color::Green, Color::Purple, Color::Red, Color::White, Color::Yellow
+        Color::Blue,
+        Color::Cyan,
+        Color::Green,
+        Color::Purple,
+        Color::Red,
+        Color::White,
+        Color::Yellow,
     ];
 
-    let color_map: HashMap<(usize,usize),Color> = basins.iter().zip( colours.into_iter().cycle() ).flat_map( |(basin,color)| basin.into_iter().map( move |&pos| (pos,color) ) ).collect();
-    let minima: HashSet<(usize,usize)> = map.minima().map( |(pos,_)| pos ).collect();
+    let color_map: HashMap<(usize, usize), Color> = basins
+        .iter()
+        .zip(colours.into_iter().cycle())
+        .flat_map(|(basin, color)| basin.into_iter().map(move |&pos| (pos, color)))
+        .collect();
+    let minima: HashSet<(usize, usize)> = map.minima().map(|(pos, _)| pos).collect();
 
-    let border = ansi_term::Style::new().bold().on( Color::Black );
+    let border = ansi_term::Style::new().bold().on(Color::Black);
 
-    for y in 0 .. map.height {
-        for x in 0 .. map.width {
-            let v = map.height_at( &x, &y ).unwrap();
-            let style = color_map.get( &(x,y) ).map( |&color| {
-                if minima.contains( &(x,y) ) {
-                    ansi_term::Style::new().fg( color ).bold()
-                } else {
-                    ansi_term::Style::new().fg( color ).dimmed()
-                }
-            } ).unwrap_or( border );
+    for y in 0..map.height {
+        for x in 0..map.width {
+            let v = map.height_at(&x, &y).unwrap();
+            let style = color_map
+                .get(&(x, y))
+                .map(|&color| {
+                    if minima.contains(&(x, y)) {
+                        ansi_term::Style::new().fg(color).bold()
+                    } else {
+                        ansi_term::Style::new().fg(color).dimmed()
+                    }
+                })
+                .unwrap_or(border);
 
-            print!( "{}", style.paint( format!( "{}", v ) ) );
+            print!("{}", style.paint(format!("{}", v)));
         }
         println!("")
     }
